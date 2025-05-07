ed7397b56080ecad5a8fa768076742e005001376
Mauritz Verbeke <Mauritz.Verbeke@student.tsaam.be>
2025-05-07 11:13:07 +0200

    Animaties toegevoegd

--- sluis-react/src/App.jsx
+++ sluis-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import ControlPanel from './components/ControlPanel';
 import "./styled.css";
@@ -6,14 +6,32 @@ import "./light-styled.css";
 import WaterSector from "./components/WaterSector";
 import SluisDeur from "./components/SluisDeur";
 import Stoplichten from "./components/Stoplichten";
+import { AnimatePresence, motion } from 'framer-motion';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.4 }}>
+            <Home />
+          </motion.div>
+        } />
+        <Route path="/control" element={
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.4 }}>
+            <ControlPanel />
+          </motion.div>
+        } />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/control" element={<ControlPanel />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
